186b4fa42dbf99eb1bf63a59fb6ca6f1f49d025e
unknown <katariaharsh8026@gmail.com>
2024-02-09 00:52:01 +0530

    Personal Expenses

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import './transaction_item.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function deleteTx;
+  
+  TransactionList(this.transactions, this.deleteTx);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return transactions.isEmpty 
+          ? LayoutBuilder(builder: (ctx, constraints){
+            return Column(
+              children: [
+                Text(
+                  'No Transactions added yet!', 
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                    'lib/assets/images/594816.png', 
+                    fit: BoxFit.cover,
+                )),
+              ],
+            );  
+          }) 
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return TransactionItem(
+                  transaction: transactions[index],
+                  deleteTx: deleteTx,
+                );
+              },
+        itemCount: transactions.length,
+      );
+  }
+}
